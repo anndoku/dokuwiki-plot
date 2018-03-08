eeb21d0c4f0c092ec961fa4284bb5f79cd37178a
annprog <tecbbs@qq.com>
2018-03-08 19:27:14 +0800

    fix bug of urlencode

--- syntax.php
+++ syntax.php
@@ -79,8 +79,7 @@ class syntax_plugin_plot extends DokuWiki_Syntax_Plugin {
             }
         }
         
-        $return['org_input'] = join("\n", $lines);
-        $return['input'] = urlencode($return['org_input']);
+        $return['input'] = join("\n", $lines);
         // store input for later use
         return $return;
     }
@@ -88,7 +87,7 @@ class syntax_plugin_plot extends DokuWiki_Syntax_Plugin {
     function cal_file_name($data) {
         $path = substr($this->getConf('api'),0,-7) . "cache/images/";
         $flag = str_replace(":", "_", $data['layout']);
-        return $path . md5($data['org_input']) . $flag . "." .$data['chof'];
+        return $path . md5($data['input']) . $flag . "." .$data['chof'];
     }
 
     /**
